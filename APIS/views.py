6459cf5599dd49337daa3d3671c7cb3e5436fcff
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-04-10 14:40:33 +0300

    add  login credentintials

--- APIS/views.py
+++ APIS/views.py
@@ -3,3 +3,16 @@ from . forms import Registration
 
 # Create your views here.
 
+def register(request):
+  if request.method == 'POST':
+    form = Registration(request.POST)
+    if form.is_valid():
+      form.save()
+      email = form.cleaned_data['email']
+      username = form.cleaned_data.get('username')
+
+      messages.success(request,f'Account for {username} created,you can now login')
+      return redirect('login')
+  else:
+    form = Registration()
+  return render(request,'auth/registration.html',{"form":form})
